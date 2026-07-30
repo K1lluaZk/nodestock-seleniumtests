7b0abc0c7cf4ff7509c9d8271dceb355823ffea2
Mario Suero <mario.suerodejesus@gmail.com>
2026-07-29 22:48:33 -0400

    test: añadiendo funcion para que se hagan captures automaticos por tests

--- conftest.py
+++ conftest.py
@@ -1,8 +1,10 @@
 import pytest
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+import pytest
+from datetime import datetime
 
 
 @pytest.fixture
@@ -17,3 +19,23 @@ def driver():
     yield driver
 
     driver.quit()
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call":
+
+        driver = item.funcargs.get("driver")
+
+        if driver:
+
+            os.makedirs("capturas", exist_ok=True)
+
+            nombre = f"{item.name}_{datetime.now().strftime('%Y%m%d_%H%M%S')}.png"
+
+            driver.save_screenshot(
+                f"capturas/{nombre}"
+            )
